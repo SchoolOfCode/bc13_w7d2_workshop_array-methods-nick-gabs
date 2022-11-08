bfb88d1c0715ef97de0e78c9cfd4b1dbe69f160f
Mrbusy <nickhughes13@googlemail.com>
2022-11-08 13:46:21 +0000

    Task 2b done

--- task2.js
+++ task2.js
@@ -12,8 +12,8 @@ let bListAnimals = animals.filter(bListing);
 function bListing(animal) {
   return animal.startsWith('b')
 }
-console.log(bListing);
-console.log(animal);
+console.log(animals);
+console.log(bListAnimals);
 //You have an array of lovely animals. However, you can only keep the animals that start with the letter `b`. 
 
 //👉 2a. In the new variable `bListAnimals`, use the `.filter()` method to generate a new array which contains only the animals whose name begins with 'b'. 
