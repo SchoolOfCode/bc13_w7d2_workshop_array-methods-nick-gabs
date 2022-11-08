9f9f98d4daaedde963088040793ef95ec962d9db
Gabriel Brocklesby <gabrielbrocklesby@Gabriels-MacBook-Pro.local>
2022-11-08 12:33:20 +0000

    task 2 attempt

--- task2.js
+++ task2.js
@@ -8,4 +8,14 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter(bListing); 
+function bListing(animal) {
+  return animal.startsWith('b')
+}
+console.log(bListing);
+console.log(animal);
+//You have an array of lovely animals. However, you can only keep the animals that start with the letter `b`. 
+
+//👉 2a. In the new variable `bListAnimals`, use the `.filter()` method to generate a new array which contains only the animals whose name begins with 'b'. 
+
+
